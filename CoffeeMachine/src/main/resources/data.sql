36ad03d2392500a25fdb6e074e033ce4ea5f9354
Prikhozhaya Katya <eap@lab321.ru>
2022-05-15 23:28:42 +0600

    Added settings to the database.

---
+++ CoffeeMachine/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO coffee_drink (name_coffee_drink, weight_grain, weight_water) VALUES ('покрепче', 10, 10)^;
+INSERT INTO coffee_drink (name_coffee_drink, weight_grain, weight_water) VALUES ('мягкий', 5, 10)^;
+INSERT INTO coffee_drink (name_coffee_drink, weight_grain, weight_water) VALUES ('душистый', 7, 10)^;
+INSERT INTO resource_container (name, weight) VALUES ('вода', 1000)^;
+INSERT INTO resource_container (name, weight) VALUES ('зерна', 100)^;
