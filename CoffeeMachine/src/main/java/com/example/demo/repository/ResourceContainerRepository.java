e5d9e0cac0643131ba09c9e178d23c666634a040
Prikhozhaya Katya <eap@lab321.ru>
2022-05-15 18:56:12 +0600

    Added Repository.

---
+++ CoffeeMachine/src/main/java/com/example/demo/repository/ResourceContainerRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.ResourceContainer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResourceContainerRepository extends JpaRepository<ResourceContainer, Long> {
+}
